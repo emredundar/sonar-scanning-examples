ac1ed8b34ec13e20a68a5dc0aca5d495c3471b86
Emre <emredundar.e@gmail.com>
2018-05-22 17:03:09 +0300

    Update HelloWorld.java

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -1,14 +1,6 @@
 package example;
 
 public class HelloWorld {
-  
-  HelloWorld(HelloWorld hw) { }
-  int     foo1() { return 0; }
-  void    foo2(int value) { }
-  int     foo3(int value) { return 0; } // Noncompliant
-  Object  foo4(int value) { return null; }
-  HelloWorld foo5(HelloWorld value) {return null; } // Noncompliant
-  int     foo6(int value, String name) { return 0; }
 
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
